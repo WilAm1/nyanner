38b4d18f28e89ea3a456e8861999f22c50319050
Wil <wilhelmjanmapogi@gmail.com>
2022-03-23 13:48:27 +0800

    Add simple components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { BrowserRouter, Router, Route } from "react-router-dom";
+import SignIn from "./components/SignIn";
+
+// * High Level TODOS and MVP
+// *    Sign In / Sign Out
+// *    Able to read,write,edit, and delete own tweets
+// *    Display the first 10 recent posts
+// *    Heart/Upvote liked tweets
+
+// * Working Log
+// TODO Default the path if no login found to Signin
+// TODO Have home component
+// TODO configure firebase
+// TODO configure context API for user auth
+// TODO Make mock api calls
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Router>
+        <Route path="/" element={<SignIn />} />
+      </Router>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/SignIn.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SignIn() {
+  return <div>Hello world</div>;
+}
+
+export default SignIn;
