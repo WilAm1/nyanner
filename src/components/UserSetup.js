66486736c6c0937ee44ae329bd46b7e1565e6032
Wil <wilhelmjanmapogi@gmail.com>
2022-03-27 10:22:42 +0800

    Add UserSetup component

---
+++ src/components/UserSetup.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+function UserSetup() {
+  const [signUpDetails, setSignUpDetails] = useState({
+    userName: "",
+    name: "",
+  });
+
+  const handleChange = (e) => {
+    const inputName = e.target.name;
+    setSignUpDetails((s) => {
+      return { ...s, [inputName]: e.target.value };
+    });
+  };
+
+  const handleSubmit = () => {
+    console.log(signUpDetails);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="userName"
+          placeholder="user name"
+          onChange={handleChange}
+        />
+        <button type="button">Check Availability</button>
+        <input
+          type="text"
+          name="name"
+          placeholder="display name"
+          onChange={handleChange}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default UserSetup;
